e5853ee06c1902b0657dedab65c9f98539826e04
Otávio Araújo <otaviotech@gmail.com>
2023-11-22 07:35:06 +0000

    feat: add kitty and lazyvim config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,4 +1,2 @@
-require("otaviotech.plugins.plugins-setup")
-require("otaviotech.core.options")
-require("otaviotech.core.keymaps")
-require("otaviotech.core.colorscheme")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
