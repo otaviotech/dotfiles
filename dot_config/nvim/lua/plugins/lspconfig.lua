3929dd57f7d1ca1e01fdf57a2be3c43d6e4b6960
Otávio Pardim <otaviotech@gmail.com>
2024-12-04 02:18:36 +0000

    feat: revamp tmux and neovim

--- dot_config/nvim/lua/plugins/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -1,17 +1,6 @@
 return {
-	"neovim/nvim-lspconfig",
-	opts = {
-		inlay_hints = { enabled = false },
-		servers = {
-			gopls = {
-				settings = {
-					gopls = {
-            gofumpt = true,
-						env = { GOFLAGS = "-tags=unit,integration" },
-						analyses = { fieldalignment = false, unusedparams = true },
-					},
-				},
-			},
-		},
-	},
+  "nvim-lspconfig",
+  opts = {
+    inlay_hints = { enabled = false },
+  },
 }
--- dot_config/nvim/lua/plugins/rainbow.lua
+++ dot_config/nvim/lua/plugins/rainbow.lua
@@ -1,3 +1,19 @@
 return {
-	"mechatroner/rainbow_csv",
+  "cameron-wags/rainbow_csv.nvim",
+  config = true,
+  ft = {
+    "csv",
+    "tsv",
+    "csv_semicolon",
+    "csv_whitespace",
+    "csv_pipe",
+    "rfc_csv",
+    "rfc_semicolon",
+  },
+  cmd = {
+    "RainbowDelim",
+    "RainbowDelimSimple",
+    "RainbowDelimQuoted",
+    "RainbowMultiDelim",
+  },
 }
